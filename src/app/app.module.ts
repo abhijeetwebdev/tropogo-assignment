ba890ea1a7facdfac0e807e9f97956e6ddd8ecd9
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2020-05-30 16:59:43 +0530

    add: institute form comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
+
+// forms
+import { InstituteForm } from './forms/institute/institute.form';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProgressbarComponent
+    ProgressbarComponent,
+    InstituteForm
   ],
   imports: [
     BrowserModule,
