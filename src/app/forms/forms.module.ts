ab9a7c4a627434430da2c951744124d0343d9053
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-05-30 20:35:53 +0530

    add: dynamic form field builder

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule as NgFormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // forms
 import { InstituteForm } from './institute/institute.form';
 
+// components
+import { FieldBuilder } from './field-builder';
+
+// input fields
+import { TextBoxComponent } from './fields/textbox';
+import { FileComponent } from './fields/file';
+
 @NgModule({
   declarations: [
-    InstituteForm
+    InstituteForm,
+    FieldBuilder,
+    TextBoxComponent,
+    FileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgFormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    InstituteForm
+    InstituteForm,
+    FieldBuilder
   ]
 })
 export class FormsModule { }
