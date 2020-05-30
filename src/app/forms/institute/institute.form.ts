3acdc8d23b9a11d9054b6d36201bb80ef593cbdf
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-05-30 16:59:43 +0530

    add: institute form comp

---
+++ src/app/forms/institute/institute.form.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-institute-form',
+  templateUrl: './institute.form.html',
+  styleUrls: ['./institute.form.scss']
+})
+export class InstituteForm implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
