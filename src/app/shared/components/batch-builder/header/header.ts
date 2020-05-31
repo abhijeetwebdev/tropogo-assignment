98335f7ca2446c060923bc0b415269cb495227f6
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-05-31 14:31:30 +0530

    add: datepicker

--- src/app/shared/components/batch-builder/header/header.ts
+++ src/app/shared/components/batch-builder/header/header.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'batch-header',
@@ -9,6 +10,9 @@ export class BatchHeader implements OnInit {
     @Input() index: any;
     @Input() batch: any;
     @Input() form: any;
+
+    startDate: NgbDateStruct;
+    endDate: NgbDateStruct;
 
     constructor() { }
 
