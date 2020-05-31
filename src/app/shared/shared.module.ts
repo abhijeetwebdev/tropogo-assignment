c8b081c0567efaffc62caec776d6060b6f547728
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-05-31 12:27:45 +0530

    update: moved reusable comps to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule as NgFormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
+import { BatchBuilder } from './components/batch-builder/batch-builder';
+import { FieldBuilder } from './components/field-builder/field-builder';
+
+// fields
+import { TextBoxComponent } from './components/field-builder/fields/textbox';
+import { FileComponent } from './components/field-builder/fields/file';
+import { RadioComponent } from './components/field-builder/fields/radio';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    ProgressbarComponent
+    ProgressbarComponent,
+    FieldBuilder,
+    TextBoxComponent,
+    FileComponent,
+    RadioComponent,
+    BatchBuilder
   ],
   imports: [
     CommonModule,
-    NgbModule
+    NgbModule,
+    NgFormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     NavbarComponent,
-    ProgressbarComponent
+    ProgressbarComponent,
+    FieldBuilder,
+    TextBoxComponent,
+    FileComponent,
+    RadioComponent,
+    BatchBuilder
   ]
 })
 export class SharedModule { }
