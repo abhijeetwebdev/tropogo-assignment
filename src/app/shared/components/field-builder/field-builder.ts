4f2671b394ca03b1f095fbab436ff336b3cd3df2
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-06-01 17:29:39 +0530

    update: page 1 improvements

--- src/app/shared/components/field-builder/field-builder.ts
+++ src/app/shared/components/field-builder/field-builder.ts
@@ -3,21 +3,7 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-field-builder',
-  template: `
-    <div class="form-group row" [formGroup]="form">
-      <label class="col-md-3 form-control-label" [attr.for]="field.label">
-        {{field.label}}
-        <strong *ngIf="!field.required">(optional)</strong>
-      </label>
-      <div class="col-md-9" [ngSwitch]="field.type">
-        <textbox *ngSwitchCase="'text'" [field]="field" [form]="form"></textbox>
-        <radio *ngSwitchCase="'radio'" [field]="field" [form]="form"></radio>
-        <file *ngSwitchCase="'file'" [field]="field" [form]="form"></file>
-        <div class="alert alert-danger my-1 p-2 fadeInDown animated"
-          *ngIf="showError && (field.value === '' || field.files?.length === 0) && field.required"
-        >{{field.label}} is required</div>
-      </div>
-    </div>`
+  templateUrl: './field-builder.html'
 })
 export class FieldBuilder implements OnInit, OnDestroy {
 
