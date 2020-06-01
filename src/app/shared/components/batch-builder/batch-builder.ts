e8aed04c9584bb1aee84badefc554150d925ddd5
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2020-06-01 12:40:56 +0530

    update: preserve dates within the batches

--- src/app/shared/components/batch-builder/batch-builder.ts
+++ src/app/shared/components/batch-builder/batch-builder.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Batch } from 'src/app/models/batch.model';
 
 @Component({
     selector: 'app-batch-builder',
@@ -8,7 +10,7 @@ import { Subscription } from 'rxjs';
 export class BatchBuilder implements OnInit, OnDestroy {
 
     @Input() showError: any;
-    @Input() batches: any;
+    @Input() batches: Batch[];
     @Input() form: any;
     batchSub: Subscription;
 
@@ -28,5 +30,11 @@ export class BatchBuilder implements OnInit, OnDestroy {
 
     delete(index: number) {
         this.batches.splice(index, 1);
+    }
+
+    setDate(data: { index: number, type: string, date: NgbDateStruct }) {
+        if (this.batches[data.index]) {
+            this.batches[data.index][data.type] = data.date;
+        }
     }
 }
