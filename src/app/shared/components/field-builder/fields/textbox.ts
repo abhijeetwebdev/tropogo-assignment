f3e431d6230d5e6f4c4b6bfed1e267c1d57aebaa
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2020-06-01 17:35:38 +0530

    update: UI improvements

--- src/app/shared/components/field-builder/fields/textbox.ts
+++ src/app/shared/components/field-builder/fields/textbox.ts
@@ -3,29 +3,7 @@ import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'textbox',
-  template: `
-  <div [formGroup]="form">
-    <label
-      class="mt-3"
-      [class.color-danger]="showError && field.value === '' && field.required"
-    >{{ field.label }}</label>
-    <input class="form-control"
-      *ngIf="!field.multiline"
-      [attr.type]="field.type"
-      [id]="field.name"
-      [name]="field.name"
-      [formControlName]="field.name"
-      [placeholder]="field.placeholder"
-    >
-    <textarea class="form-control"
-      *ngIf="field.multiline"
-      [class.is-invalid]="isDirty && !isValid"
-      [formControlName]="field.name"
-      [id]="field.name"
-      rows="9"
-      [placeholder]="field.placeholder"
-    ></textarea>
-  </div>`
+  templateUrl: './textbox.html'
 })
 export class TextBoxComponent {
   
