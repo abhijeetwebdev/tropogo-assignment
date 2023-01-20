1f0c033198c9d77dbd801cebc7992bc679300ef7
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:43:15 +0530

    add: required field validators

--- src/app/forms/field-builder.ts
+++ src/app/forms/field-builder.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-field-builder',
@@ -11,19 +12,31 @@ import { Component, Input, OnInit } from '@angular/core';
       <div class="col-md-9" [ngSwitch]="field.type">
         <textbox *ngSwitchCase="'text'" [field]="field" [form]="form"></textbox>
         <file *ngSwitchCase="'file'" [field]="field" [form]="form"></file>
-        <div class="alert alert-danger my-1 p-2 fadeInDown animated" *ngIf="!isValid && isDirty">{{field.label}} is required</div>
+        <div class="alert alert-danger my-1 p-2 fadeInDown animated"
+          *ngIf="showError && (field.value === '' || field.files?.length === 0) && field.required"
+        >{{field.label}} is required</div>
       </div>
     </div>`
 })
-export class FieldBuilder implements OnInit {
+export class FieldBuilder implements OnInit, OnDestroy {
 
+  @Input() showError: any;
   @Input() field: any;
   @Input() form: any;
+  fieldSub: Subscription;
 
   get isValid() { return this.form.controls[this.field.name].valid; }
   get isDirty() { return this.form.controls[this.field.name].dirty; }
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.fieldSub = this.form.controls[this.field.name].valueChanges.subscribe((value) => {
+      this.field.value = value;
+    });
+  }
+
+  ngOnDestroy() {
+    this.fieldSub.unsubscribe();
+  }
 }
